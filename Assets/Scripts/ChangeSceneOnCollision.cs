5a51fa5452024e5964fa8e5a5af0eb558f32f831
rshdd <raschidllamas@hotmail.com>
2024-07-04 19:17:42 -0600

    Added scene looping and made game look better

---
+++ Assets/Scripts/ChangeSceneOnCollision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DuckGameSceneTrigger"))
+        {
+            SceneManager.LoadScene("DuckShootingScene");
+        }
+
+        if (other.CompareTag("CornholoGameSceneTrigger"))
+        {
+            SceneManager.LoadScene("CornholeScene");
+        }
+
+        if (other.CompareTag("LobbyGameSceneTrigger"))
+        {
+            SceneManager.LoadScene("LobbyScene");
+        }
+    }
+}
